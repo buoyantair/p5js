c790d30b051a3c8e7bbede2c21678e3a2d2349cd
buoyantair <buoyantair@gmail.com>
2017-02-25 14:12:57 +0530

    Made a scene!

--- week1/sketch.js
+++ week1/sketch.js
@@ -1,9 +1,8 @@
 function setup() {
     createCanvas(640, 360);
-}
-
-function draw() {
+    
     background(161,255,254);
+    
     fill(185,229,150);
     stroke(0,0);
     rect(0,260,640,100);
@@ -12,4 +11,8 @@ function draw() {
     ellipse(185,260,150,70);
     ellipse(0,260,300,130);
     ellipse(600,300,300,200);
+}
+
+function draw() {
+    
 }
