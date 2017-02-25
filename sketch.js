d25cb31a76d2f8daca6a0b0c18679497912b1a3b
buoyantair <buoyantair@gmail.com>
2017-02-25 12:45:32 +0530

    Added p5js example folder to begin with

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(640, 360);
+}
+
+function draw() {
+    background(255,0,0);
+    ellipse(200, 200, 50, 50);
+    rect(100,200,75,150);
+    line(100,200,10,20);
+}
