f4b9bf22e4098ec3db96a1c8e033c2b583bdaa74
buoyantair <buoyantair@gmail.com>
2017-02-25 13:44:00 +0530

    Week1

---
+++ week1/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(640, 360);
+}
+
+function draw() {
+    background(161,255,254);
+    fill(185,229,150);
+    stroke(0,0);
+    rect(0,260,640,100);
+    
+    
+    ellipse(185,260,150,70);
+    ellipse(0,260,300,130);
+    ellipse(600,300,300,200);
+}
