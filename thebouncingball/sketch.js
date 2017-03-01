a189843a02499a5dcb6452ca05745473fc07849d
buoyantair <buoyantair@gmail.com>
2017-03-01 20:54:52 +0530

    Bounce!

--- thebouncingball/sketch.js
+++ thebouncingball/sketch.js
@@ -1,24 +1,33 @@
 var pageHeight = window.innerHeight;
 var pageWidth = window.innerHeight;
-var x = 0;
 
+var ball = {
+    x: 0,
+    y: 0,
+    xspeed: 10,
+    yspeed: -7
+}
 
 
 function setup() {
     createCanvas(pageWidth, pageHeight);
 }
 
-
 function draw() {
     background(0);
-    stroke(255);
-    strokeWeight(4);
-    noFill();
-    ellipse(x, 200, 100, 100);
 
-    if (x > width){
-        x= 0;
-    } else{
-        x+= 3;
+
+    if (ball.x > width || ball.x < 0){
+        ball.xspeed = ball.xspeed * -1;
     }
+
+    if (ball.y > height || ball.y < 0){
+        ball.yspeed = ball.yspeed * -1;
+    }
+
+
+    ball.x = ball.x + ball.xspeed;
+    ball.y = ball.y + ball.yspeed;
+
+    ellipse(ball.x, ball.y, 100, 100);
 }
